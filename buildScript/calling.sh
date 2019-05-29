d6ac4f0421c75e1239a025378779a302b0fbc8e6
sato yoshiro <ys2519@nifty.com>
2019-05-29 19:56:21 +0900

    calling.shについて、コマンド実行時に引数を渡すように対応 スクリプト実行時に、指定番号に発信するように対応

--- buildScript/calling.sh
+++ buildScript/calling.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
 # Account Setting
-user="101"
-pass="######"
-RegistServer="178.128.29.94"
+# ex)
+# ./calling.sh 178.128.29.94 101 password 0353211111
+#
+#registServer="178.128.29.94"
+#user="101"
+#pass="password"
+#tel="0353211111"
+registServer=$1
+user=$2
+pass=$3
+tel=$4
 
 echo "Start Calling"
 
-#echo -n RegistServer:
-#read RegistServer
-echo Connect To:${RegistServer}
+echo connectTo:${registServer}
+echo user:${user}
+echo tel:${tel}
 
-param="--id=sip:101@${RegistServer} --registrar=sip:asrterisk@${RegistServer} --realm=Asterisk --username=${user} --password=${pass} --local-port=23355"
+param="--id=sip:101@${registServer} --registrar=sip:asrterisk@${registServer} --realm=Asterisk --username=${user} --password=${pass} --local-port=23355 sip:${tel}@${registServer}"
 echo ${param}
-read -p "Press [Enter] key to resume."
+#read -p "Press [Enter] key to resume."
 
 ./pjsua-x86_64-unknown-linux-gnu ${param}
-
-#m
-#sip:0353211111@178.128.29.94
 
 echo "Complete!"
